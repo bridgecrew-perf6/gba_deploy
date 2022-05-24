c1ecf21c03c4656155fd03c7ffc92a720ce09bbf
Bigalbal <reivin.bigalbal@accenture.com>
2022-05-24 10:27:03 +0800

    Updates

---
+++ precache-manifest.50d296c2666bd325021c0aa5171fd377.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bca8c6f2dd166da61f54",
+    "url": "/gba_deploy/static/css/main.8328e5c1.chunk.css"
+  },
+  {
+    "revision": "bca8c6f2dd166da61f54",
+    "url": "/gba_deploy/static/js/main.bca8c6f2.chunk.js"
+  },
+  {
+    "revision": "8f1f9bdd8571ce5e7bb7",
+    "url": "/gba_deploy/static/js/1.8f1f9bdd.chunk.js"
+  },
+  {
+    "revision": "0f5afa402788489f0ff2",
+    "url": "/gba_deploy/static/css/2.10284343.chunk.css"
+  },
+  {
+    "revision": "0f5afa402788489f0ff2",
+    "url": "/gba_deploy/static/js/2.0f5afa40.chunk.js"
+  },
+  {
+    "revision": "bcb4c9f82c2c43ffd50a",
+    "url": "/gba_deploy/static/js/runtime~main.bcb4c9f8.js"
+  },
+  {
+    "revision": "bdfaf4f417cb6e1f3e2486bc77e191d7",
+    "url": "/gba_deploy/index.html"
+  }
+];
